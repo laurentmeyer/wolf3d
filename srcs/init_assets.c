311749bc8855f32883ac50147da8abb4634c0c7c
Lcharvol <charvolin0@gmail.com>
2018-10-15 17:37:05 +0200

    Fix init_assets.c

--- srcs/init_assets.c
+++ srcs/init_assets.c
@@ -6,7 +6,7 @@
 /*   By: lcharvol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/15 12:48:21 by lcharvol          #+#    #+#             */
-/*   Updated: 2018/10/15 12:48:30 by lcharvol         ###   ########.fr       */
+/*   Updated: 2018/10/15 17:36:48 by lcharvol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,26 @@
 #include "display.h"
 #include "geometry.h"
 
-/*
-** les textures sont stockées colonne par colonne et non ligne par ligne
-*/
-
 t_texture		init_texture(t_ram *ram, t_image *img, int id)
 {
 	t_texture	ret;
-	const t_v2i	orig = (t_v2i){(id % 6) * TEX_WIDTH, (id / 6) * TEX_HEIGHT};
+	t_v2i		orig;
 	t_v2i		cur;
 
-	if (NULL == (ret = (t_texture)malloc(TEX_WIDTH * TEX_HEIGHT * sizeof(int))))
+	orig = (t_v2i){(id % 6) * (int)TEX_WIDTH, (id / 6) *
+		(int)TEX_HEIGHT};
+	if (NULL == (ret = (t_texture)malloc((int)TEX_WIDTH * (int)TEX_HEIGHT *
+					sizeof(int))))
 		exit_message(ram, ERROR, "Could not allocate texture\n");
-	ft_bzero(ret, TEX_WIDTH * TEX_HEIGHT * sizeof(int));
+	ft_bzero(ret, (int)TEX_WIDTH * (int)TEX_HEIGHT * sizeof(int));
 	cur = orig;
-	while (cur.x - orig.x < TEX_WIDTH)
+	while (cur.x - orig.x < (int)TEX_WIDTH)
 	{
 		cur.y = orig.y;
-		while (cur.y - orig.y < TEX_HEIGHT)
+		while (cur.y - orig.y < (int)TEX_HEIGHT)
 		{
-			ft_memcpy(ret + TEX_WIDTH * (cur.x - orig.x) + (cur.y - orig.y),
-				img->data_addr + img->chars_per_pixel *
+			ft_memcpy(ret + (int)TEX_WIDTH * (cur.x - orig.x) +
+					(cur.y - orig.y), img->data_addr + img->chars_per_pixel *
 				(img->pixels_per_line * cur.y + cur.x), img->chars_per_pixel);
 			(cur.y)++;
 		}
@@ -44,12 +43,13 @@ t_texture		init_texture(t_ram *ram, t_image *img, int id)
 	return (ret);
 }
 
-void	init_textures(t_ram *ram)
+void			init_textures(t_ram *ram)
 {
-	t_image	img;
-	int		i;
+	t_image		img;
+	int			i;
 
-	if (NULL == (ram->assets->textures = (t_texture *)malloc(TEX_COUNT * sizeof(t_texture))))
+	if (NULL == (ram->assets->textures = (t_texture *)malloc((int)TEX_COUNT *
+					sizeof(t_texture))))
 		exit_message(ram, ERROR, "Could not initialize textures array\n");
 	ft_bzero(ram->assets->textures, TEX_COUNT * sizeof(t_texture));
 	if (NULL == (img.img_ptr = mlx_xpm_file_to_image(ram->display->mlx_ptr,
@@ -60,40 +60,41 @@ void	init_textures(t_ram *ram)
 	img.chars_per_pixel = img.bits_per_pixel / 8;
 	img.pixels_per_line = img.size_line / img.chars_per_pixel;
 	i = -1;
-	while (++i < TEX_COUNT)
+	while (++i < (int)TEX_COUNT)
 		ram->assets->textures[i] = init_texture(ram, &img, i);
 	mlx_destroy_image(ram->display->mlx_ptr, img.img_ptr);
 }
 
-void	init_map_layout(t_ram *ram)
+void			init_map_layout(t_ram *ram)
 {
-	int		i;
-	int		j;
-	int		c;
+	int			i;
+	int			j;
+	int			c;
 
-	if (NULL == (ram->assets->map_layout = (int *)malloc(MMAP_PXPM * MMAP_PXPM *
-		ram->world->map_h * ram->world->map_w * sizeof(int))))
+	if (NULL == (ram->assets->map_layout = (int *)malloc((int)MMAP_PXPM *
+		(int)MMAP_PXPM * ram->world->map_h * ram->world->map_w * sizeof(int))))
 		exit_message(ram, ERROR, "Could not initialize minimap template\n");
 	ft_bzero(ram->assets->map_layout, MMAP_PXPM * MMAP_PXPM *
 		ram->world->map_h * ram->world->map_w * sizeof(int));
-	i = 0;
-	while (i < ram->world->map_h * MMAP_PXPM)
+	i = -1;
+	while (i++ < ram->world->map_h * (int)MMAP_PXPM)
 	{
-		j = 0;
-		while (j < ram->world->map_w * MMAP_PXPM)
+		j = -1;
+		while (j++ < ram->world->map_w * (int)MMAP_PXPM)
 		{
-			c = (ram->world->map)[ram->world->map_w * (i / MMAP_PXPM) + (j / MMAP_PXPM)];
+			c = (ram->world->map)[ram->world->map_w * (i / (int)MMAP_PXPM)
+				+ (j / (int)MMAP_PXPM)];
 			if (TEX_EMPTY == c)
-				(ram->assets->map_layout)[ram->world->map_w * i * MMAP_PXPM + j] = 0x383838;
+				(ram->assets->map_layout)[ram->world->map_w * i
+					* (int)MMAP_PXPM + j] = 0x383838;
 			else
-				(ram->assets->map_layout)[ram->world->map_w * i * MMAP_PXPM + j] = 0x707070;
-			j++;
+				(ram->assets->map_layout)[ram->world->map_w * i
+					* (int)MMAP_PXPM + j] = 0x707070;
 		}
-		i++;
 	}
 }
 
-void		init_assets(t_ram *ram)
+void			init_assets(t_ram *ram)
 {
 	if (NULL == (ram->assets = (t_assets *)malloc(sizeof(t_assets))))
 		exit_message(ram, ERROR, "Could not initialize assets\n");
@@ -102,9 +103,9 @@ void		init_assets(t_ram *ram)
 	init_map_layout(ram);
 }
 
-void	free_assets(t_assets *assets)
+void			free_assets(t_assets *assets)
 {
-	int	i;
+	int			i;
 
 	if (NULL != assets->textures)
 	{
